9334db983d063cbbfa11576df3bca8549589daaf
Elmer Balbin Jr <contact@elmerbalb.in>
2020-09-04 21:26:19 +0800

    Final edits

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Validation from './Validation/Validation';
 import Char from './Char/Char';
 import './Char/Char.css';
+import Assignment from './Assignment/Assignment';
+import './Assignment/Assignment.css';
 
 class App extends Component {
   state = {
@@ -77,40 +79,7 @@ class App extends Component {
             <Validation length={this.state.length} />
             <p>Character Count: {this.state.length}</p>
             {characters}
-            <div className="assignmentDetails">
-              <code>
-                <ol>
-                  <li>
-                    Create an input field (in App component) with a change
-                    listener which outputs the length of the entered text below
-                    it (e.g. in a paragraph).
-                  </li>
-                  <li>
-                    Create a new component (=> ValidationComponent) which
-                    receives the text length as a prop
-                  </li>
-                  <li>
-                    Inside the ValidationComponent, either output "Text too
-                    short" or "Text long enough" depending on the text length
-                    (e.g. take 5 as a minimum length)
-                  </li>
-                  <li>
-                    Create another component (=> CharComponent) and style it as
-                    an inline box (=> display: inline-block, padding: 16px,
-                    text-align: center, margin: 16px, border: 1px solid black).
-                  </li>
-                  <li>
-                    Render a list of CharComponents where each CharComponent
-                    receives a different letter of the entered text (in the
-                    initial input field) as a prop.
-                  </li>
-                  <li>
-                    When you click a CharComponent, it should be removed from
-                    the entered text.
-                  </li>
-                </ol>
-              </code>
-            </div>
+            <Assignment />
           </div>
         </header>
       </div>
